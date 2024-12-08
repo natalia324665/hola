9336ad6684e23fab2053b9ac29ab64f70e99fd76
jerezn912 <jerezn912@gmail.com>
2024-12-08 15:12:17 +0000

    teorema del valor medio

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import streamlit as st
 #1) crear las paginas
-uno= st.Page("paginas/uno.py",title="presentacion",icon=":material/star:")
-dos= st.Page("paginas/dos.py",title="teorema del valor medio",icon="🌞")
-tres= st.Page("paginas/tres.py",title="graficas",icon="🐇")
-cuatro= st.Page("paginas/cuatro.py",title="evaluacion",icon="🌜")
-cinco= st.Page("paginas/cinco.py",title="referencias",icon="📚")
+ uno= st.Page("paginas/uno.py",title="presentacion",icon=":material/star:")
+ dos= st.Page("paginas/dos.py",title="teorema del valor medio",icon="🌞")
+ tres= st.Page("paginas/tres.py",title="graficas",icon="🐇")
+ cuatro= st.Page("paginas/cuatro.py",title="evaluacion",icon="🌜")
+ cinco= st.Page("paginas/cinco.py",title="referencias",icon="📚")
 
 
-
-
-#2)crear la navegacion 
-pg = st.navigation({"prueba":[uno,dos,],"ejemplos":[tres],"cuatro":[cuatro],"cinco":[cinco]})
+ #2)crear la navegacion 
+ pg = st.navigation({"prueba":[uno,dos,],"ejemplos":[tres],"cuatro":[cuatro],"cinco":[cinco]})
 
 #iniciar la aplicacion
-pg.run()
+ pg.run()
